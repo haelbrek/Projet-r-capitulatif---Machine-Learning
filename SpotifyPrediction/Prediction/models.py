6c2c759511012414d17d8a7cae79e3cd44d67a70
JynMoriarty <108009430+JynMoriarty@users.noreply.github.com>
2023-02-23 09:16:40 +0100

    feat: :tada: Création du django
    Création du django + de la page inscription
    
    le notebook test a été ajouté mais il sera supprimé plus tard

---
+++ SpotifyPrediction/Prediction/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Prediction(models.Model):
+
+    tracks = models.CharField(null=False,max_length=30)
+    duration_ms = models.PositiveIntegerField()
+    explicit = models.PositiveIntegerField()
+    danceability = models.FloatField()
+    energy  = models.FloatField()
+    key = models.PositiveIntegerField()
+    loudness= models.PositiveIntegerField()
+    mode =models.PositiveIntegerField()
+    speechiness = models.FloatField()
+    acousticness = models.FloatField()
+    instrumentalness = models.FloatField()
+    liveness = models.FloatField()
+    valence =models.FloatField()
+    tempo = models.FloatField()
+    time_signature = models.PositiveIntegerField()
